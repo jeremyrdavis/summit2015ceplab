14a28ac0760b1b58d63f0ab604dc438aee5d7c4c
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2015-05-31 07:39:39 -0400

    lab3 working

--- lab3/src/test/java/com/redhat/summit2015/ceplab/Lab3Test.java
+++ lab3/src/test/java/com/redhat/summit2015/ceplab/Lab3Test.java
@@ -10,9 +10,11 @@ import org.drools.core.time.SessionPseudoClock;
 import org.junit.Before;
 import org.junit.Test;
 import org.kie.api.KieServices;
+import org.kie.api.definition.type.FactType;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.rule.EntryPoint;
+import org.kie.api.runtime.rule.FactHandle;
 
 import com.redhat.summit2015.ceplab.model.Account;
 import com.redhat.summit2015.ceplab.model.AccountStatus;
@@ -49,28 +51,37 @@ public class Lab3Test extends TestCase {
 	}
 
 	@Test
-	public void testSomething() {
+	public void testSomething() throws InstantiationException, IllegalAccessException {
 		assertNotNull("kSession should be instantiated", kSession);
         assertNotNull("EntryPoint should not be null", entryPoint);
 		
 		Account account = new Account(AccountStatus.ACTIVE, BigDecimal.valueOf(10000));
-		Transaction withdrawl1 = new Transaction(account, BigDecimal.valueOf(100));
-		Transaction withdrawl2 = new Transaction(account, BigDecimal.valueOf(100));
-		Transaction withdrawl3 = new Transaction(account, BigDecimal.valueOf(100));
+		Transaction withdrawl1 = new Transaction(account, BigDecimal.valueOf(400));
+		Transaction withdrawl2 = new Transaction(account, BigDecimal.valueOf(400));
+		Transaction withdrawl3 = new Transaction(account, BigDecimal.valueOf(150));
+
+		FactType accountInfoFactType = kSession.getKieBase().getFactType("com.redhat.summit2015.ceplab", "AccountInfo");
+		Object accountInfo = accountInfoFactType.newInstance();
+		accountInfoFactType.set(accountInfo, "averageBalance",
+				BigDecimal.valueOf(1000));
+		accountInfoFactType.set(accountInfo, "id", account.getId());
+		FactHandle accountInfoHandle = kSession.insert(accountInfoFactType);
+		kSession.update(accountInfoHandle, accountInfo);		
 
 		kSession.insert(account);
 		entryPoint.insert(withdrawl1);
 		entryPoint.insert(withdrawl2);
 		entryPoint.insert(withdrawl3);
 		
-		
+
 		kSession.fireAllRules();
-		
+
 		assertSame("Account should not be blocked", account.getStatus(), AccountStatus.BLOCKED);
+		
 	}
 	
 	@Test
-	public void testPassingWithdrawls(){
+	public void testPassingWithdrawls() throws InstantiationException, IllegalAccessException{
 		assertNotNull("kSession should be instantiated", kSession);
         assertNotNull("EntryPoint should not be null", entryPoint);
 		
@@ -87,7 +98,6 @@ public class Lab3Test extends TestCase {
 		System.out.println(clock.getCurrentTime());
 		
 		entryPoint.insert(withdrawl2);
-
 		entryPoint.insert(withdrawl3);
 		
 		kSession.fireAllRules();
