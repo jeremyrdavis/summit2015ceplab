41174976338955c0f5de1be5eef27240dfa2649c
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2015-06-18 10:33:05 -0400

    moved setUp code to parent class

--- lab2/src/test/java/com/redhat/summit2015/ceplab/Lab2Test.java
+++ lab2/src/test/java/com/redhat/summit2015/ceplab/Lab2Test.java
@@ -1,35 +1,10 @@
 package com.redhat.summit2015.ceplab;
 
 import java.math.BigDecimal;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import junit.framework.TestCase;
-
-import org.drools.core.ClockType;
-import org.drools.core.time.SessionPseudoClock;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.kie.api.KieBase;
-import org.kie.api.KieBaseConfiguration;
-import org.kie.api.KieServices;
-import org.kie.api.builder.KieBuilder;
-import org.kie.api.builder.KieFileSystem;
-import org.kie.api.builder.Message;
-import org.kie.api.builder.Message.Level;
-import org.kie.api.builder.ReleaseId;
-import org.kie.api.builder.model.KieBaseModel;
-import org.kie.api.builder.model.KieModuleModel;
-import org.kie.api.builder.model.KieSessionModel;
-import org.kie.api.builder.model.KieSessionModel.KieSessionType;
-import org.kie.api.conf.EventProcessingOption;
-import org.kie.api.runtime.KieContainer;
-import org.kie.api.runtime.KieSession;
-import org.kie.api.runtime.KieSessionConfiguration;
-import org.kie.api.runtime.conf.ClockTypeOption;
 import org.kie.api.runtime.rule.EntryPoint;
-import org.kie.internal.io.ResourceFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,90 +14,32 @@ import com.redhat.summit2015.ceplab.model.Transaction;
 import com.redhat.summit2015.ceplab.model.TransactionStatus;
 
 /**
- * Unit test for simple App.
+ * Base TestCase that contains relevant member variables and handles the
+ * boilerplate for building KieSessions
+ * 
+ * @author jeremy.davis@redhat.com
  */
-public class Lab2Test extends TestCase {
+public class Lab2Test extends BaseCEPTestCase {
 
 	private static final Logger LOGGER = LoggerFactory
 			.getLogger(Lab2Test.class);
 
-	KieSession kSession;
-
-	@Before
+	@Override
 	public void setUp() {
-
-		KieServices kieServices = KieServices.Factory.get();
-		KieModuleModel kieModuleModel = kieServices.newKieModuleModel();
-
-		KieBaseModel kieBaseModel = kieModuleModel
-				.newKieBaseModel("lab2KieBase").setDefault(true)
-				.setEventProcessingMode(EventProcessingOption.STREAM);
-		KieSessionModel kieSessionModel = kieBaseModel
-				.newKieSessionModel("lab2KieSession")
-				.setClockType(ClockTypeOption.get("pseudo"))
-				.setType(KieSessionType.STATEFUL).setDefault(true);
-		
-
-		KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
-		kieFileSystem.write(ResourceFactory
-				.newClassPathResource("rules/transaction1.drl"));
-		kieFileSystem.writeKModuleXML(kieModuleModel.toXML());
-
-		KieBuilder kieBuilder = kieServices.newKieBuilder(kieFileSystem);
-		kieBuilder.buildAll();
-
-		List<Message> errorMessages = kieBuilder.getResults().getMessages(
-				Level.ERROR);
-		for (Message m : errorMessages) {
-			System.out.println(m);
-		}
-		assertEquals("There shouldn't be any errors", kieBuilder.getResults()
-				.getMessages(Level.ERROR).size(), 0);
-
-		ReleaseId releaseId = kieBuilder.getKieModule().getReleaseId();
-		
-		KieContainer kcontainer = kieServices.newKieContainer(releaseId);
-
-		KieBase kieBase = kcontainer.getKieBase("lab2KieBase");
-
-		// Configure and create the KieSession
-		kSession = kcontainer.newKieSession("lab2KieSession");
-
-		// KieBaseConfiguration kieBaseConfiguration =
-		// kieServices.newKieBaseConfiguration();
-		// KieBase kieBase = kcontainer.newKieBase(kieBaseConfiguration);
-		//
-		// KieSessionConfiguration kieSessionConfiguration =
-		// kieServices.newKieSessionConfiguration();
-		// kieSessionConfiguration.set
-
-		// KieContainer kContainer = ks.getKieClasspathContainer();
-		// kSession = kContainer.newKieSession();
-
-		ClockTypeOption clockType = kSession.getSessionConfiguration()
-				.getOption(ClockTypeOption.class);
-		assertSame(clockType.getClockType(), ClockType.PSEUDO_CLOCK.toString());
-
-		assertNotNull(
-				"kSession should be instantiated and set to member variable",
-				kSession);
-	}
-
-	@After
-	public void tearDown() {
-		kSession.dispose();
-		kSession.destroy();
+		setDrls("rules/transaction1.drl");
+		super.setUp();
 	}
 
 	@Test
 	public void testSomething() {
-		assertNotNull("kSession should be instantiated", kSession);
 
-		SessionPseudoClock clock = kSession.getSessionClock();
-		assertNotNull("SessionClock should not be null", clock);
+		// extra sanity check
+		assertNotNull("kSession should be instantiated", kSession);
 
 		EntryPoint entryPoint = kSession.getEntryPoint("Transfers");
 		assertNotNull("EntryPoint should not be null", entryPoint);
+
+		LOGGER.debug("basic setup okay");
 
 		Account fromAccount = new Account();
 		Account toAccount = new Account();
@@ -148,9 +65,6 @@ public class Lab2Test extends TestCase {
 	@Test
 	public void test15SecondRule() {
 		assertNotNull("kSession should be instantiated", kSession);
-
-		SessionPseudoClock clock = kSession.getSessionClock();
-		assertNotNull("SessionClock should not be null", clock);
 
 		EntryPoint entryPoint = kSession.getEntryPoint("Transfers");
 
