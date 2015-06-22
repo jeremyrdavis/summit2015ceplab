c3c280d56f6d4519f7e5d3995b102f219eae0347
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2015-06-22 06:39:26 -0400

    Lab4 refactored

--- lab4/src/test/java/com/redhat/summit2015/ceplab/Lab4Test.java
+++ lab4/src/test/java/com/redhat/summit2015/ceplab/Lab4Test.java
@@ -58,35 +58,4 @@ public class Lab4Test extends BaseCEPTestCase{
         assertSame("the fifth withdrawl should be denied", t5.getStatus(), TransactionStatus.DENIED);
     }
     
-    @Test
-    public void testSuspiciousTransactionOver100IsDenied(){
-        assertNotNull("kSession should be instantiated", kSession);
-        kSession.fireAllRules();
-
-        Account fromAccount = new Account(AccountStatus.ACTIVE, BigDecimal.valueOf(5000));
-        
-        Transaction t1 = new Transaction(fromAccount, new BigDecimal("100"));
-        Transaction t2 = new Transaction(fromAccount, new BigDecimal("100"));
-        Transaction t3 = new Transaction(fromAccount, new BigDecimal("100"));
-        Transaction t4 = new Transaction(fromAccount, new BigDecimal("100"));
-        Transaction t5 = new Transaction(fromAccount, new BigDecimal("200"));
-        entryPoint.insert(t1);
-        clock.advanceTime(1, TimeUnit.SECONDS);
-        entryPoint.insert(t2);
-        clock.advanceTime(1, TimeUnit.SECONDS);
-        entryPoint.insert(t3);
-        clock.advanceTime(1, TimeUnit.SECONDS);
-        entryPoint.insert(t4);
-        clock.advanceTime(1, TimeUnit.SECONDS);
-        entryPoint.insert(t5);
-        kSession.fireAllRules();
-        
-        assertNotSame("the first transaction should not be denied", t1.getStatus(), TransactionStatus.DENIED);
-        assertNotSame("the second transaction should not be denied", t2.getStatus(), TransactionStatus.DENIED);
-        assertNotSame("the third transaction should not be denied", t3.getStatus(), TransactionStatus.DENIED);
-        assertNotSame("the fourth transaction should not be denied", t4.getStatus(), TransactionStatus.DENIED);
-
-        // t5 is denied because it is twice the medium of last 4 transactions (t1, t2, t3, t4)
-        assertSame("the fifth withdrawl should be denied", t5.getStatus(), TransactionStatus.DENIED);
-    }
 }
