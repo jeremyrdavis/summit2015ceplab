b8334c36d89e25005fdea527fc37b0c190d78cbf
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2015-06-22 06:29:31 -0400

    Lab3 refactored

--- lab3/src/test/java/com/redhat/summit2015/ceplab/Lab3Test.java
+++ lab3/src/test/java/com/redhat/summit2015/ceplab/Lab3Test.java
@@ -57,37 +57,4 @@ public class Lab3Test extends BaseCEPTestCase {
 		
 	}
 	
-	@Test
-	public void testPassingWithdrawls() throws InstantiationException, IllegalAccessException{
-		assertNotNull("kSession should be instantiated", kSession);
-        assertNotNull("EntryPoint should not be null", entryPoint);
-		
-		Account account = new Account(AccountStatus.ACTIVE, BigDecimal.valueOf(10000));
-		Transaction withdrawl1 = new Transaction(account, BigDecimal.valueOf(100));
-		Transaction withdrawl2 = new Transaction(account, BigDecimal.valueOf(100));
-		Transaction withdrawl3 = new Transaction(account, BigDecimal.valueOf(100));
-		
-		FactType accountInfoFactType = kSession.getKieBase().getFactType("com.redhat.summit2015.ceplab", "AccountInfo");
-		Object accountInfo = accountInfoFactType.newInstance();
-		accountInfoFactType.set(accountInfo, "averageBalance",
-				BigDecimal.valueOf(10000));
-		accountInfoFactType.set(accountInfo, "id", account.getId());
-		FactHandle accountInfoHandle = kSession.insert(accountInfoFactType);
-		kSession.update(accountInfoHandle, accountInfo);		
-
-		kSession.insert(account);
-		entryPoint.insert(withdrawl1);
-		
-		System.out.println(clock.getCurrentTime());
-		clock.advanceTime(20, TimeUnit.MINUTES);
-		System.out.println(clock.getCurrentTime());
-		
-		entryPoint.insert(withdrawl2);
-		entryPoint.insert(withdrawl3);
-		
-		kSession.fireAllRules();
-		
-		assertSame("Account should still be Active", account.getStatus(), AccountStatus.ACTIVE);
-	}
-
 }
